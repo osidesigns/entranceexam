330dd20d90de67662ecba46dcb541ceb22056b1c
Steven Andrews <osidesigns@gmail.com>
2018-10-22 20:10:36 -0500

    Temp index.php file

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>FS | Entrance Exam</title>
+</head>
+<body>
+
+	<form>
+		<input type="radio" name="answer" id="a" value="a" /> A <br />
+		<input type="radio" name="answer" id="b" value="b" /> B <br />
+		<input type="radio" name="answer" id="c" value="c" /> C <br />
+		<input type="radio" name="answer" id="d" value="d" /> D <br />
+		<input type="submit" id="submitBtn" value="Submit Answer" />
+	</form>
+	
+	<script src="js/jsFunctions.js"></script>
+</body>
+</html>
+
