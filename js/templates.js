78fc9fbe21da4e6c0d748f1d2b83346085d4b341
Matthew Carstensen <sonofcarsten@gmail.com>
2018-10-22 23:04:16 -0500

    Added js/templates.js
    Current version will write the HTML for ul#answers

---
+++ js/templates.js
@@ -0,0 +1,10 @@
+function answerTemplate(_question_id, _i, _text){
+    let answers = ["a", "b", "c", "d", "e"];
+
+    return `
+    <li class="answer-item list-group-item">	
+        <input type="radio" name="answer" value="${answers[_i]}" class="answer">
+        <label for="${answers[_i]}">${answers[_i].toUpperCase}</label>
+        <span id="answer-${_question_id}-${_i}">${_text}</span>
+    </li>`
+}
